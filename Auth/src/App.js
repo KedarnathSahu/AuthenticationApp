d5ed0077112254adaed2bcc085db891e30d477cb
KedarnathSahu <kedarnath.sahu97@gmail.com>
2019-02-06 02:14:03 +0530

    integrated firebase auth

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+import firebase from '@firebase/app';
 
-export default class App extends Component {
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCwFwW04lIiGg2QcriboLi2dp5u1SnljGs',
+      authDomain: 'authentication-9c40d.firebaseapp.com',
+      databaseURL: 'https://authentication-9c40d.firebaseio.com',
+      projectId: 'authentication-9c40d',
+      storageBucket: 'authentication-9c40d.appspot.com',
+      messagingSenderId: '493565089552',
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View >
         <Header headerText="Authentication" />
-        <Text >Welcome to Auth App!</Text>
+        <LoginForm />
       </View>
     );
   }
 }
+
+export default App;
